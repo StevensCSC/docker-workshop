bb429d61f8732a45d2458c56399c82d1afdb1594
Robert Herley <rherley@stevens.edu>
2018-10-10 16:58:32 -0400

    added some more slides

---
+++ theme.js
@@ -0,0 +1,14 @@
+import theme from 'mdx-deck/themes';
+
+export default {
+   ...theme,
+   font: 'Lato',
+   colors: {
+      text: '#525f7f',
+      background: 'white',
+      link: 'tomato'
+   },
+   heading: {
+      color: '#3ba0ee'
+   }
+};
